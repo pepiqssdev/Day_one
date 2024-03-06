bfd51e75f4c04f81997f9e597d58d64141feffe8
Fernet <peps.ntwari@gmail.com>
2024-03-06 17:09:09 +0200

    Dayone

---
+++ bin/first_dart.dart
@@ -0,0 +1,28 @@
+void main () {
+  var firstna = 'Fernet';
+
+  String  firstn= 'Kevin';
+
+  Object firstname = 'Gracia';
+
+  //you can tell the differences for those data types(var, string, object)
+  print (firstna ); // print string value
+  int a= 23;
+  int b = 78;
+  int c= a+b;//La somme de a et b
+
+ print(c);// print integer value
+  double d=7.34;
+
+  print(d); // print double value
+
+  const e= 90; //constate value
+  final f= 800; //final value
+
+  print (e); // print constante value
+  print (f); //print final value
+
+  // Difference bettween constante and fianly
+
+
+}// main method
